199255bbe953b6615a6065c4aa3997a5ed8208f3
SamPardoe <107503209+SamPardoe@users.noreply.github.com>
2024-10-26 16:47:22 +0100

    Update client.sh
    Added comments and removed commented-out code

--- w4/client.sh
+++ w4/client.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#get username and server to connect to
 usrname=${1}
 svrname=${2}
 svr=/tmp/chatsvr${svrname}.txt
@@ -7,33 +8,27 @@ usrpos=${#clientArr[@]}
 echo "Your position is $usrpos"
 #add user to the client list
 echo $usrname >> /tmp/${svrname}clients
+#print messages
 connected=1
 latest=0
 while [[ $connected -ne 0 ]]; do
-  #read instr
-  #if [[ $instr == "piss-off" ]]; then
-  #  echo "Quitting server...asshole."
-  #  (cat /tmp/${svrname}clients | sed "s/$usrname/disconnected/") > /tmp/${svrname}clients
-  #  connected=0
-  #  break
-  #fi
   if [[ ! -f /tmp/${svrname}clients ]]; then
     continue
   fi
-  #print to screen the values from server
-  IFS=$'\r\n' GLOBIGNORE='*' command eval clientArr='($(cat /tmp/${svrname}clients))'
-  #echo balls
+  #only print if there is a new message
+  IFS=$'\r\n' GLOBIGNORE='*' command eval clientArr='($(cat /tmp/${svrname}clients))' #update client list
   last=$latest
   latest=$(tail -n 1 $svr)
   from=${latest: -1}
-  #echo $from , ${clientArr[$from]}
   (if [[ ! $latest == $last ]]; then
     (echo ${latest%?} | sed "s/^/${clientArr[$from]} @ $(date +%H:%M:%S) > /") &
     fi) &
+  #read input in background
   (read instr
   if [[ -z $instr ]]; then
     continue
   else
+  #to disconnect from the server
     if [[ $instr == "piss-off" ]]; then
       echo "Quitting server...asshole."
       (cat /tmp/${svrname}clients | sed "s/$usrname/disconnected/") > /tmp/${svrname}clients
