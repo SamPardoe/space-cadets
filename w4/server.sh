ae15b46c17b02c5b0c13be673b8df6c6faab97fd
SamPardoe <107503209+SamPardoe@users.noreply.github.com>
2024-10-26 16:39:16 +0100

    Update server.sh
    Added comments and removed commented-out code

--- w4/server.sh
+++ w4/server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+#Get server name & initialise server & client list
 svrname=${1}
 svr=/tmp/chatsvr${svrname}.txt
-#[ -p $svr ] || touch $svr
 if [[ -f $svr ]]; then
   rm $svr
 fi
@@ -9,26 +9,19 @@ touch $svr
 if [[ -f /tmp/${svrname}clients ]]; then
   rm /tmp/${svrname}clients
 fi
-#write: use -1 index? need to write to all clients
-#get clients
-#everything in whilerunning loop
+#print messages
 running=1
 latest=0
 while [[ $running -ne 0 ]]; do
   if [[ ! -f /tmp/${svrname}clients ]]; then
     continue
   fi
-  IFS=$'\r\n' GLOBIGNORE='*' command eval clientArr='($(cat /tmp/${svrname}clients))'
+  IFS=$'\r\n' GLOBIGNORE='*' command eval clientArr='($(cat /tmp/${svrname}clients))' #update client list
+  #only print if there is a new message
   last=$latest
   latest=$( tail -n 1 $svr)
   from=${latest: -1}
   if [[ ! $latest == $last ]]; then
     (echo ${latest%?} | sed "s/^/${clientArr[$from]} @ $(date +%H:%M:%S) > /")
   fi
-  #(echo "Connected clients: ${clients[@]}" ; cat) >> $svr
-  #if [[ ${#clientArr[@]} ]]; then
-  #  echo "No users present in the server, shutting down."
-  #  running=0
-  #fi
 done
-#rm /tmp/chatsvr${svrname}.txt
